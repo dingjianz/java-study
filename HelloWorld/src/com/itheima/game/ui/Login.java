cd0f6e043959c4c148a75fcef60294bb92097c26
dingjian <jianding9@iflytek.com>
2026-03-12 23:27:36 +0800

    完善登录界面：添加用户列表参数传递和用户名长度验证

--- HelloWorld/src/com/itheima/game/ui/Login.java
+++ HelloWorld/src/com/itheima/game/ui/Login.java
@@ -1,8 +1,13 @@
 package com.itheima.game.ui;
 
+import com.itheima.game.bean.User;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Login {
+
+
     // 登录界面
     public void start() {
         System.out.println("|--------------------------|");
@@ -10,16 +15,18 @@ public class Login {
         System.out.println("|__________________________|");
         System.out.println("请选择操作：1登录 2注册 3退出");
 
+        ArrayList<User> list = new ArrayList<>();
+
         Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
 
-        while(true) {
+        while (true) {
             switch (choice) {
                 case 1:
-                    login();
+                    login(list);
                     break;
                 case 2:
-                    register();
+                    register(list);
                     break;
                 case 3:
                     System.out.println("用户选择了退出");
@@ -27,24 +34,34 @@ public class Login {
                     break;
                 default:
                     System.out.println("输入有误，请重新输入");
-                    choice = sc.nextInt();
                     break;
             }
         }
     }
 
     // 登录
-    public void login() {
-        System.out.println("请输入用户名：");
+    public void login(ArrayList<User> list) {
         Scanner sc = new Scanner(System.in);
-        String username = sc.next();
+        while (true) {
+            System.out.println("请输入用户名：");
+            String username = sc.next();
+            if (checkLen(3, 6, username)) {
+                System.out.println("用户名的长度必须是3-16位");
+                continue;
+            }
+        }
         System.out.println("请输入密码：");
         String password = sc.next();
         System.out.println("用户名是：" + username + "，密码是：" + password);
     }
 
     // 注册
-    public void register() {
+    public void register(ArrayList<User> list) {
 
+    }
+
+    // 判断字符的长度是否在指定的范围之内
+    public boolean checkLen(int minLen, int maxLen, String str) {
+        return  str.length() >= minLen && str.length() <= maxLen;
     }
 }
